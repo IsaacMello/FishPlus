7f8097aa9d722ec30f5e6c4bb761d2ee7b9ab6f6
Isaac Mello <ifms@ic.ufal.br>
2022-01-27 03:52:41 -0300

    Add files via upload

---
+++ main/java/com/project/fishplus/FormCadastro.java
@@ -0,0 +1,15 @@
+package com.project.fishplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+    }
+}
